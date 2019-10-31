25892370afd9cfa7251e3342630444420063d411
Wenjie Zhang <zhang_a_a_a@outlook.com>
2019-10-31 09:16:45 +0800

    modify test

--- mypointeranalysis/test.sh
+++ mypointeranalysis/test.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
 set -e
-set -x
+# set -x
+
+TEST_CASE=$1
+if [ "$TEST_CASE"x == x ]; then
+    TEST_CASE=test.Hello
+fi
 
 mvn compile
 mvn dependency:copy-dependencies
 mvn -Dclassifier=sources dependency:copy-dependencies
 
 CLASSPATH=$(JARS=(target/dependency/*.jar); IFS=:; echo "${JARS[*]}"):target/classes
-echo 'xxx'
-java -cp $CLASSPATH com.mypointeranalysis.MyPointerAnalysis ../code test.Hello
+
+echo 'Start Test'
+echo '---------------------------------'
+java -cp $CLASSPATH com.mypointeranalysis.MyPointerAnalysis ../code $TEST_CASE
