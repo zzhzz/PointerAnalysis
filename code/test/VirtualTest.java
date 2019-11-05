4d5c6fcc44a44bed4b169e768264bd8ff1e94524
Wenjie Zhang <19354817+myxxxsquared@users.noreply.github.com>
2019-11-05 15:49:30 +0800

    x

--- code/test/VirtualTest.java
+++ code/test/VirtualTest.java
@@ -5,7 +5,7 @@ import benchmark.objects.*;
 public class VirtualTest
 {
     public VirtualTest a, b, c, d, e, f;
-    public static int main(String agrv[]) {
+    public static void main(String[] args) {
         P p = new P(null);
         Q q = p;
         benchmark.internal.BenchmarkN.test(123, p);
@@ -18,6 +18,5 @@ public class VirtualTest
         }
         P[][] p2 = new P[3][];
         System.out.println(p2);
-        return 0;
     }
 }
