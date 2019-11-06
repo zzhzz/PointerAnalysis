e7a22b3aa8669a10d5f77948da870d2ba69650cf
Wenjie Zhang <19354817+myxxxsquared@users.noreply.github.com>
2019-11-06 22:23:28 +0800

    add testcases

---
+++ code/test/RecRef.java
@@ -0,0 +1,18 @@
+package test;
+
+import benchmark.internal.BenchmarkN;
+import benchmark.objects.A;
+
+public class RecRef {
+    RecRef r;
+    public static void main(String[] args) {
+        BenchmarkN.alloc(1);
+        RecRef r1 = new RecRef();
+        BenchmarkN.alloc(2);
+        RecRef r2 = new RecRef();
+        r1.r = r2;
+        r2.r = r1;
+        RecRef r = r1.r.r.r.r.r.r.r.r.r;
+        BenchmarkN.test(1, r); // {1}
+    }
+}
